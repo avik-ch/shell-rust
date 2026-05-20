9d391cb0fede76645180673ba4d62b9fb95bc7d8
avik-ch <181900631+avik-ch@users.noreply.github.com>
2026-05-19 21:03:32 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+
+    let mut input = String::new();
+
     print!("$ ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    print!("{}: command not found", input.trim());
     io::stdout().flush().unwrap();
 }
