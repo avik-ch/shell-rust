b108568c0a539cf0d651459c9f917202c7805c2c
avik-ch <181900631+avik-ch@users.noreply.github.com>
2026-05-19 21:38:10 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const KNOWN_COMMANDS: [&str; 3] = ["exit", "echo", "type"];
+
 fn main() {
     // stage 3
     loop {
@@ -23,7 +25,16 @@ fn main() {
         match command {
             "exit" => break,
             "echo" => println!("{}", args),
+            "type" => get_type(args),
             _ => println!("{}: command not found", input.trim()),
         }
+    }
+}
+
+fn get_type(arg: &str) {
+    if KNOWN_COMMANDS.contains(&arg) {
+        println!("{arg} is a shell builtin");
+    } else {
+        println!("{arg}: not found");
     }
 }
