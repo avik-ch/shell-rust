e3044adc7596fcf2bc49ec9a13acc55f1b72703a
avik-ch <181900631+avik-ch@users.noreply.github.com>
2026-06-18 16:36:32 -0500

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -8,11 +8,11 @@ pub fn tokenise(line: &str) -> Result<Vec<String>, &'static str> {
 
     while let Some(&letter) = letters.peek() {
         match letter {
-            '\'' => {
-                letters.next();
-                let (mut arg, closed) = parse_quote(&mut letters);
+            '\'' | '"' => {
+                let quote = letters.next().unwrap();
+                let (mut arg, closed) = parse_quote(&mut letters, quote);
                 if !closed {
-                    get_remaining_quote(&mut arg);
+                    get_remaining_quote(&mut arg, quote);
                 }
                 cur_arg.push_str(&arg);
             }
@@ -32,11 +32,11 @@ pub fn tokenise(line: &str) -> Result<Vec<String>, &'static str> {
     Ok(args)
 }
 
-fn parse_quote(letters: &mut impl Iterator<Item = char>) -> (String, bool) {
+fn parse_quote(letters: &mut impl Iterator<Item = char>, quote: char) -> (String, bool) {
     let mut cur_arg = String::new();
     while let Some(letter) = letters.next() {
         match letter {
-            '\'' => return (cur_arg, true),
+            x if x == quote => return (cur_arg, true),
             _ => cur_arg.push(letter),
         }
     }
@@ -44,7 +44,7 @@ fn parse_quote(letters: &mut impl Iterator<Item = char>) -> (String, bool) {
     (cur_arg, false)
 }
 
-fn get_remaining_quote(arg: &mut String) {
+fn get_remaining_quote(arg: &mut String, quote: char) {
     let mut input = String::new();
     loop {
         print!("quote> ");
@@ -53,7 +53,7 @@ fn get_remaining_quote(arg: &mut String) {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        let (new_arg, closed) = parse_quote(&mut input.trim().chars().peekable());
+        let (new_arg, closed) = parse_quote(&mut input.trim().chars().peekable(), quote);
         arg.push_str(&new_arg);
         if closed {
             break;
